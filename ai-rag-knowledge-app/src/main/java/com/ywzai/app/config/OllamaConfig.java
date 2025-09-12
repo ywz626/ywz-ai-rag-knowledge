4acd19ce4a77af81b1afedbd72ff5edd9fa510da
ywz626 <1461507285@qq.com>
2025-09-12 19:17:31 +0800

    feat(app): 实现知识库功能并添加测试用例
    - 新增向量存储和文档分割相关配置
    - 添加 Postgres 向量存储和 Ollama嵌入客户端的 Bean
    - 新增知识库测试用例，包括文档上传和聊天功能
    - 添加测试文档文件

--- ai-rag-knowledge-app/src/main/java/com/ywzai/app/config/OllamaConfig.java
+++ ai-rag-knowledge-app/src/main/java/com/ywzai/app/config/OllamaConfig.java
@@ -1,10 +1,16 @@
 package com.ywzai.app.config;
 
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.OllamaEmbeddingClient;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class OllamaConfig {
@@ -17,6 +23,25 @@ public class OllamaConfig {
     @Bean
     public OllamaChatClient ollamaChatClient(OllamaApi ollamaApi) {
         return new OllamaChatClient(ollamaApi);
+    }
+
+    @Bean
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+    @Bean
+    public SimpleVectorStore simpleVectorStore(OllamaApi ollamaApi) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new SimpleVectorStore(embeddingClient);
+    }
+
+    @Bean
+    public PgVectorStore pgVectorStore(JdbcTemplate jdbcTemplate,OllamaApi ollamaApi) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        //指定使用的向量模型
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new PgVectorStore(jdbcTemplate,embeddingClient);
     }
 
 }
